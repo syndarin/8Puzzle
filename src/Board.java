e94d5f875ce3ac5c1b2a08887ea69288b10a33fb
Viacheslav Tiahotenkov <vtiahotenkov@cogniance.com>
2017-02-16 12:57:12 +0200

    Implement methods of Board except twin()

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,33 @@
-import edu.princeton.cs.algs4.MinPQ;
+import java.util.Arrays;
 
 public class Board {
 	
 	private final int DIMENSION;
+	private final int BLANK_FIELD_INDEX;
+	
+	private int[] blocks;
+	
+	private int emptyBlockIndex;
 	
 	public Board(int[][] blocks) {
 		DIMENSION = blocks.length;
+		
+		this.blocks = new int[DIMENSION * DIMENSION];
+
+		for (int i = 0; i < DIMENSION; i++) {
+			for (int j = 0; j < DIMENSION; j++) {
+				
+				int index = i * DIMENSION + j; 
+				
+				this.blocks[index] = blocks[i][j];
+				
+				if (this.blocks[index] == 0) {
+					emptyBlockIndex = index;
+				}
+			}
+		}
+		
+		BLANK_FIELD_INDEX = this.blocks.length - 1;
 	}
 
 	public int dimension() {
@@ -13,15 +35,52 @@ public class Board {
 	}
 
 	public int hamming() {
-		return 0;
+		int result = 0;
+		
+		for (int i = 0; i < blocks.length; i++) {
+			if (blocks[i] != 0 && blocks[i] != i + 1) {
+				result++;
+			}
+		}
+		
+		return result;
 	}
 
 	public int manhattan() {
-		return 0;
+		int result = 0;
+		
+		for (int i = 0; i < blocks.length; i++) {
+			if (blocks[i] != 0 && blocks[i] != i + 1) {
+				result += getManhattanDistance(i, blocks[i] - 1);
+			}
+		}
+		
+		return result;
+	}
+	
+	private int getManhattanDistance(int fromPosition, int toPosition) {
+		int absLength = Math.abs(fromPosition - toPosition);
+		return absLength / DIMENSION + absLength % DIMENSION;
 	}
 
 	public boolean isGoal() {
-		return false;
+		boolean result = true;
+		
+		if (emptyBlockIndex != BLANK_FIELD_INDEX) {
+			
+			result = false;
+			
+		} else {
+			
+			for (int i = 0, max = blocks.length - 1; i < max; i++) {
+				if (blocks[i] != i + 1) {
+					result = false;
+					break;
+				}
+			}
+		}
+		
+		return result;
 	}
 
 	public Board twin() {
@@ -29,7 +88,14 @@ public class Board {
 	}
 
 	public boolean equals(Object y) {
-		return false;
+		if (this == y) {
+			return true;
+		} else if (y == null || !(y instanceof Board)) {
+			return false;
+		} else {
+			Board that = (Board) y;
+			return Arrays.equals(this.blocks, that.blocks);
+		}
 	}
 
 	public Iterable<Board> neighbors() {
@@ -37,7 +103,47 @@ public class Board {
 	}
 
 	public String toString() {
-		return null;
+		StringBuilder builder = new StringBuilder();
+		builder.append("\n");
+		for (int i = 0; i < blocks.length; i++) {
+			builder.append(blocks[i] == 0 ? "    " : String.format("%4d", blocks[i]));
+			
+			if (i % DIMENSION == DIMENSION - 1) {
+				builder.append("\n");
+			}
+		}
+		
+		return builder.toString();
 	}
-
+	
+	public static void main(String[] args) {
+		int[][] blocks = new int[][]{
+			new int[]{8, 1, 3},
+			new int[]{4, 0, 2},
+			new int[]{7, 6, 5}
+		};
+		
+		int[][] otherBlocks = new int[][]{
+			new int[]{8, 1, 3},
+			new int[]{4, 0, 2},
+			new int[]{7, 6, 5}
+		};
+		
+		Board board = new Board(blocks);
+		Board otherBoard = new Board(otherBlocks);
+		
+		assert(board.equals(board));
+		assert(board.equals(otherBoard));
+		assert(!board.equals(null));
+		assert(!board.equals(new Object()));
+		
+		assert(board.dimension() == 3);
+		
+		assert(board.hamming() == 5);
+		
+		assert(board.manhattan() == 10);
+		
+		System.out.println("Tests passed");
+		System.out.println(board.toString());
+	}
 }
--- src/Solver.java
+++ src/Solver.java
@@ -8,18 +8,25 @@ public class Solver {
 		private int moves;
 		private SearchNode previous;
 
-		@Override
-		public int compareTo(SearchNode o) {
-			// TODO Auto-generated method stub
-			return 0;
+		public SearchNode(Board board, int moves, SearchNode previous) {
+			this.board = board;
+			this.moves = moves;
+			this.previous = previous;
 		}
 		
+		@Override
+		public int compareTo(SearchNode o) {
+			return board.hamming() - o.board.hamming();
+		}
 	}
 	
 	private MinPQ<SearchNode> queue;
 
 	public Solver(Board initial) {
 		queue = new MinPQ<>();
+		
+		SearchNode initialNode = new SearchNode(initial, 0, null);
+		queue.insert(initialNode);
 	}
 
 	public boolean isSolvable() {
